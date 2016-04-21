a2d6d4ee9cd86e98c489510803edd1b71a471d1d
cristiansteib <steibkhriz@gmail.com>
2016-04-21 18:22:42 -0300

    first add

---
+++ sim800l.cpp
@@ -0,0 +1,7 @@
+#include "Arduino.h"
+#include "sim800l.h"
+
+sim800l::sim800l()
+{
+	
+}
---
+++ sim800l.h
@@ -0,0 +1,11 @@
+#ifndef sim800l_h
+#define sim800l_h
+
+#include "Arduino.h"
+
+class sim800l
+{
+  public:
+	sim800l();
+	void sendSms();	
+};
